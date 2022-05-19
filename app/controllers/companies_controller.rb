f47cb4506b80f567f20cc27489e925ff8f0f5d39
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-05-19 16:03:27 +0000

    Updates

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,12 @@
 class CompaniesController < ApplicationController
 
   def index
-      @companies = Company.all
+    @companies = Company.all
   end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    @contacts = Contact.where({ "company_id" => @company["id"] })
   end
 
   def new
@@ -13,11 +14,30 @@ class CompaniesController < ApplicationController
   end
 
   def create
-    @company =Company.new
+    @company = Company.new
     @company["name"] = params["company"]["name"]
     @company["city"] = params["company"]["city"]
     @company["state"] = params["company"]["state"]
     @company.save
+    redirect_to "/companies"
+  end
+
+  def edit
+    @company = Company.find_by({ "id" => params["id"] })
+  end
+
+  def update
+    @company = Company.find_by({ "id" => params["id"] })
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
+  end
+
+  def destroy
+    @company = Company.find_by({ "id" => params["id"] })
+    @company.destroy
     redirect_to "/companies"
   end
 
